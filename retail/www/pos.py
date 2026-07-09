6b77c833fd3c552686633e48dbaca66ea72f6439
Ransford <72665919+Ransbort@users.noreply.github.com>
2026-07-09 18:22:11 +0000

    Redirect guests to login before accessing POS

--- retail/www/pos.py
+++ retail/www/pos.py
@@ -3,6 +3,17 @@ import frappe
 no_cache = 1
 
 def get_context(context):
-    context.index_html = frappe.read_file(
-        "assets/retail/retail_suite/index.html"
+    if frappe.session.user == "Guest":
+        frappe.local.response["type"] = "redirect"
+        frappe.local.response["location"] = "/login?redirect-to=/pos"
+        raise frappe.Redirect
+
+    html = frappe.read_file("assets/retail/retail_suite/index.html")
+
+    csrf_token = frappe.sessions.get_csrf_token()
+    boot_script = (
+        f'<script>window.csrf_token = "{csrf_token}";</script>'
     )
+    html = html.replace("</head>", boot_script + "</head>", 1)
+
+    context.index_html = html
